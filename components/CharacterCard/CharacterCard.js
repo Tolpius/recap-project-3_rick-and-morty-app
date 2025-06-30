13cac6ab99b70eedfad62f59c5642a4b58293b76
Martin <martkeu@web.de>
2025-06-30 15:00:08 +0200

    first solution

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,32 @@
-export function createCharacterCard() {}
+export function createCharacterCard(result) {
+	const listEl = document.createElement('li');
+
+	listEl.classList.add('card');
+
+	listEl.innerHTML = `
+        <div class="card__image-container">
+            <img
+                class="card__image"
+                src="${result.image}"
+                alt="Rick Sanchez"
+            />
+            <div class="card__image-gradient"></div>
+        </div>
+
+        <div class="card__content">
+            <h2 class="card__title">${result.name}</h2>
+            <dl class="card__info">
+                <dt class="card__info-title">Status</dt>
+                <dd class="card__info-description">${result.status}</dd>
+
+                <dt class="card__info-title">Type</dt>
+                <dd class="card__info-description">${result.type}</dd>
+
+                <dt class="card__info-title">Occurrences</dt>
+                <dd class="card__info-description">${result.episode.length}</dd>
+            </dl>
+        </div>
+    `;
+
+    return listEl;
+}
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -9,6 +7,86 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+const maxPage = 42;
+let page = 1;
+let searchQuery = '';
+
+pagination.textContent = `${page} / ${maxPage}`;
+
+/*---------------------------------------------------------------------------------
+| Fetch Data
+|----------------------------------------------------------------------------------
+| 
+*/
+const baseURL = 'https://rickandmortyapi.com/api/';
+
+async function fetchCharacters(url) {
+	cardContainer.innerHTML = '';
+
+	// const resp = await fetch('https://rickandmortyapi.com/api/character');
+	const resp = await fetch(url);
+	const data = await resp.json();
+
+	console.log(data.results);
+
+	const results = data.results;
+
+	results.forEach((result) => {
+		cardContainer.appendChild(createCharacterCard(result));
+    });
+}
+
+// fetchCharacters(baseURL + 'character');
+// fetchCharacters(baseURL + 'character?page=3');
+fetchCharacters(baseURL + `character/?name=${searchQuery}`);
+
+/*---------------------------------------------------------------------------------
+| Pagination
+|----------------------------------------------------------------------------------
+| 
+*/
+prevButton.addEventListener('click', showPreviousCharacter);
+nextButton.addEventListener('click', showNextCharacter);
+
+function showPreviousCharacter() {
+    page--;
+    
+    if (page < 1) {
+        page = 1;	
+	}
+
+    pagination.textContent = `${page} / ${maxPage}`;
+
+	fetchCharacters(baseURL + `character/?page=${page}`);
+}
+
+function showNextCharacter() {
+    page++;
+    
+    if (page > maxPage) {
+        page = maxPage;
+	}
+
+	pagination.textContent = `${page} / ${maxPage}`;
+
+	fetchCharacters(baseURL + `character/?page=${page}`);
+}
+
+
+/*---------------------------------------------------------------------------------
+| Search Bar
+|----------------------------------------------------------------------------------
+| 
+*/
+const searchInput = document.querySelector('[data-js="search-bar"] input')
+
+searchBar.addEventListener('submit', showSearchResults);
+
+function showSearchResults() {
+    searchQuery = searchInput.value;
+
+    console.log(searchQuery);
+
+    // fetchCharacters(baseURL + `character?page=${page}&name=${searchQuery}`);
+    // fetchCharacters(baseURL + `character/?name=${searchQuery}`);
+}
