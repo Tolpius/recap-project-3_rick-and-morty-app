6fff84b715427b17ecf94e66049778102072f7c1
Martin K <120308254+martkeu@users.noreply.github.com>
2025-07-01 17:37:09 +0200

    Revert "Styling"

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 import { createCharacterCard } from './components/CharacterCard/CharacterCard.js';
 
-import { NavButton } from './components/NavButton/NavButton.js';
-import { NavPagination } from './components/NavPagination/NavPagination.js';
-import { SearchBar } from './components/SearchBar/SearchBar.js';
-
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
+const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 let maxPage = 42;
 let page = 1;
 let searchQuery = '';
 
-// pagination.textContent = `${page} / ${maxPage}`;
-
-/*---------------------------------------------------------------------------------
-| Use Component-Functions
-|----------------------------------------------------------------------------------
-| 
-*/
-const prevButton = NavButton("previous", showPreviousCharacter);
-const nextButton = NavButton("next", showNextCharacter);
-const pagination = NavPagination();
-const searchBar = SearchBar((event, page) => showSearchResults(event, page));
-
-navigation.append(prevButton, pagination, nextButton);
-
-searchBarContainer.append(searchBar);
+pagination.textContent = `${page} / ${maxPage}`;
 
 /*---------------------------------------------------------------------------------
 | Fetch Data
@@ -77,6 +63,9 @@ fetchCharacters(baseURL + 'character');
 |----------------------------------------------------------------------------------
 | 
 */
+prevButton.addEventListener('click', showPreviousCharacter);
+nextButton.addEventListener('click', showNextCharacter);
+
 function showPreviousCharacter(event) {
 	page--;
 	if (page < 1) page = 1;
