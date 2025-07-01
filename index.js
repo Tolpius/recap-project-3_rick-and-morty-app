88be4dce90173adcc35c0d3e59d7b579a1c7bec4
Martin <martkeu@web.de>
2025-07-01 08:24:24 +0200

    add component-function SearchBar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { createCharacterCard } from './components/CharacterCard/CharacterCard.js
 
 import { NavButton } from './components/NavButton/NavButton.js';
 import { NavPagination } from './components/NavPagination/NavPagination.js';
+import { SearchBar } from './components/SearchBar/SearchBar.js';
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
